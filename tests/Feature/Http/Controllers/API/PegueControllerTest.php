e8e354568067a00e48393f9b3b56d49a559e68df
JustinLawrenceMS <justin.lawrence.codes@gmail.com>
2024-09-29 12:46:05 -0500

    test - api resolves and saves data

--- tests/Feature/Http/Controllers/API/PegueControllerTest.php
+++ tests/Feature/Http/Controllers/API/PegueControllerTest.php
@@ -11,6 +11,7 @@ use Tests\TestCase;
 
 class PegueControllerTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      */
@@ -28,12 +29,12 @@ class PegueControllerTest extends TestCase
 
     public function test_citation_endpoint_is_not_reachable_by_logged_out_users(): void
     {
-      //  $this->markTestSkipped('not doing api key in workflow');
+        //  $this->markTestSkipped('not doing api key in workflow');
         $response = $this->post('api/v1/citation', ['citation' => 'Ivanovic, J., Baltic, M. Z., Janjic, J., Markovic, R., Baltic, T., Boskovic, M., ... & Jovanovic, D. (2016). Health aspects of dry-cured ham. Scientific journal" Meat Technology", 57(1), 43-50.']);
         $response->assertStatus(302);
     }
 
-    public function test_api_saves_test_data(): void
+    public function test_api_post_request_resolves(): void
     {
         $citations = Storage::disk('local')->get('test.citations.json');
         $citations = json_decode($citations, true);
@@ -44,6 +45,26 @@ class PegueControllerTest extends TestCase
             $response = $this->actingAs($user)
                 ->post('api/v1/citation', ['test' => $citation]);
             $response->assertStatus(200);
+        }
+    }
+
+    public function test_api_post_request_saves_data(): void
+    {
+        $citations = Storage::disk('local')->get('test.citations.json');
+        $citations = json_decode($citations, true);
+        $user = User::factory()->create();
+
+        for ($i = 0; $i < count($citations); $i++) {
+            $citation = json_encode($citations[$i]);
+            $this->actingAs($user)
+                ->post('api/v1/citation', ['test' => $citation]);
+        }
+
+        $testCitations = Citation::all();
+
+        for ($i = 0; $i < count($testCitations); $i++) {
+            $test = json_decode($testCitations[$i]->citation, true);
+            $this->assertEquals($citations[$i], $test);
         }
     }
 }
